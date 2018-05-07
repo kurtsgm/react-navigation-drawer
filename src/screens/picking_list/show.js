b62205e8e192d1f75d69c4553fe0a9d92edc53d1
Kurt <kurtsgm@gmail.com>
2018-05-07 18:50:26 +0800

    picking list orders

--- src/screens/picking_list/show.js
+++ src/screens/picking_list/show.js
@@ -15,7 +15,11 @@ import {
   ListItem,
   ActionSheet,
   Input,
-  Footer
+  Footer,
+  Item,
+  Card,
+  CardItem,
+  Badge
 } from "native-base";
 
 
@@ -24,20 +28,34 @@ import { Grid, Col } from "react-native-easy-grid";
 import { apiFetch } from "../../api"
 import styles from "./styles";
 
-const MODE_PICK_ALL = "集結"
-const MODE_PICK_BY_ORDER = "播種"
 
 class ShowPickingList extends Component {
   constructor(props) {
     super(props)
     const { params } = this.props.navigation.state;
-    this.state = Object.assign(ShowPickingList.arrange_items([], params.items),{
+
+    let orders = {}
+    for (let order of params.orders) {
+      for (let product of order.products) {
+        for (let storage of product.storages) {
+          orders[storage.id] = orders[storage.id] || []
+          orders[storage.id].push({
+            picking_index: order.picking_index,
+            quantity: storage.quantity
+          })
+        }
+      }
+    }
+    this.state = Object.assign(ShowPickingList.arrange_items([], params.items), {
       picking_list: params,
-      mode: MODE_PICK_ALL
+      show_order: false,
+      storage_orders: orders
     })
     this.item_generator = this.item_generator.bind(this)
     this.changeQuantity = this.changeQuantity.bind(this)
+    this.changeMode = this.changeMode.bind(this)
   }
+
 
   static arrange_items(items, original_items) {
     let results = []
@@ -88,6 +106,10 @@ class ShowPickingList extends Component {
     }
   }
 
+  changeMode() {
+    this.setState({ show_order: !this.state.show_order })
+  }
+
   changeQuantity(storage_shelf_id, quantity) {
     let items = this.state.items
     let target = null
@@ -107,48 +129,44 @@ class ShowPickingList extends Component {
     return target.ready_to_pick
   }
 
-  *item_generator(data_array){
-    for(let data of data_array){
-      yield(
-        <ListItem key={data.storage_shelf_id} product_storage_id={data.product_storage_id}>
-        <Grid>
-          <Col size={4} style={styles.vertical_center} >
-            <Text style={styles.storage_title} >
-              {data.product_name}
-            </Text>
-          </Col>
-          <Col size={2} style={styles.vertical_center} >
-            <Text>
-              {data.current_shelf}
-            </Text>
-          </Col>
-          <Col size={2} style={styles.vertical_center} >
-            <Input keyboardType='numeric'
-              value={data.ready_to_pick}
-              onChangeText={
-                (text) => {
-                  let items = this.state.items
-                  for (let item of items) {
-                    if (item.storage_shelf_id == data.storage_shelf_id) {
-                      item.ready_to_pick = text
-                      break
+  *item_generator(data_array) {
+    for (let data of data_array) {
+      yield (
+        <ListItem key={data.storage_shelf_id} product_name={data.product_name} product_storage_id={data.product_storage_id}>
+          <Grid>
+            <Col size={4} style={styles.vertical_center} >
+              <Text>
+                {data.current_shelf}
+              </Text>
+            </Col>
+            <Col size={4} style={styles.vertical_center} >
+              <Input keyboardType='numeric'
+                value={data.ready_to_pick}
+                textAlign={'center'}
+                onChangeText={
+                  (text) => {
+                    let items = this.state.items
+                    for (let item of items) {
+                      if (item.storage_shelf_id == data.storage_shelf_id) {
+                        item.ready_to_pick = text
+                        break
+                      }
                     }
+                    this.setState(items)
                   }
-                  this.setState(items)
                 }
-              }
-              onEndEditing={(event) => {
-                let value = this.changeQuantity(data.storage_shelf_id, event.nativeEvent.text)
-                event.nativeEvent.text = value
-              }} value={`${data.ready_to_pick}`} returnKeyType="done" />
-          </Col>
-          <Col size={2} style={styles.vertical_center} >
-            <Button primary>
-              <Text>確認</Text>
-            </Button>
-          </Col>
-        </Grid>
-      </ListItem>
+                onEndEditing={(event) => {
+                  let value = this.changeQuantity(data.storage_shelf_id, event.nativeEvent.text)
+                  event.nativeEvent.text = value
+                }} value={`${data.ready_to_pick}`} returnKeyType="done" />
+            </Col>
+            <Col size={2} style={styles.vertical_center} >
+              <Button primary>
+                <Text>確認</Text>
+              </Button>
+            </Col>
+          </Grid>
+        </ListItem>
       )
     }
   }
@@ -160,39 +178,79 @@ class ShowPickingList extends Component {
     let current_product_storage_id
     let item_g = this.item_generator(this.state.items.sort((a, b) => a.product_storage_id - b.product_storage_id))
     let done = false
-    while(!done){
+    console.log(this.state.storage_orders)
+    while (!done) {
       let next = item_g.next()
       let _item = next.value
       done = next.done
-      if(_item){
+      if (_item) {
         current_product_storage_id = _item.props.product_storage_id
       }
 
-      if(done || prev_product_storage_id && current_product_storage_id != prev_product_storage_id ){
+
+      if (done || prev_product_storage_id && current_product_storage_id != prev_product_storage_id) {
         for (let shortage of this.state.shortage) {
           if (shortage.product_storage_id === prev_product_storage_id) {
             list_items.push(
               <ListItem key={`shortage-${shortage.product_storage_id}`}>
-                <Grid>
-                  <Col size={4} style={styles.vertical_center} >
-                    <Text style={styles.storage_title} >
-                      {shortage.product_name}
-                    </Text>
-                  </Col>
-                  <Col size={2} style={styles.vertical_center} >
-                    <Text style={styles.red}>
-                      {shortage.quantity > 0 ? '欠缺' : '過多'} 
-                      {shortage.quantity}
-                    </Text>
-                  </Col>
-                </Grid>
+                <Col size={4} style={styles.vertical_center} >
+                  <Text style={styles.red}>
+
+                    {shortage.quantity > 0 ? '欠缺' : '過多'}
+                  </Text>
+                </Col>
+                <Col size={4} style={styles.vertical_center} >
+                  <Text style={styles.red}>
+                    {shortage.quantity}
+                  </Text>
+                </Col>
+                <Col size={2} style={styles.vertical_center} >
+                </Col>
+
               </ListItem>
             )
             break
           }
         }
+        if (this.state.show_order) {
+          list_items.push(<Content padder>
+            <Card style={styles.mb} key={`card-${prev_product_storage_id}`}>
+              <CardItem header bordered>
+                <Text>訂單序號 - 商品數</Text>
+              </CardItem>
+              {
+                this.state.storage_orders[prev_product_storage_id].map((order) => {
+                  return <CardItem key={`${prev_product_storage_id}-orders-${order.picking_index}`}>
+                    <Badge
+                      style={{
+                        borderRadius: 3,
+                        height: 25,
+                        width: 72,
+                        backgroundColor: "black"
+                      }}
+                      textStyle={{ color: "white" }}
+                    >
+                      <Text>{order.picking_index}</Text>
+                    </Badge>
+
+                    <Text> - x {order.quantity} </Text>
+
+                  </CardItem>
+
+                })
+              }
+            </Card>
+          </Content>)
+        }
       }
-      if(_item){
+      if (_item && current_product_storage_id != prev_product_storage_id) {
+        list_items.push(<ListItem itemDivider key={`divider-${current_product_storage_id}`}>
+          <Text>{_item.props.product_name}</Text>
+        </ListItem>
+        )
+      }
+
+      if (_item) {
         list_items.push(_item)
       }
 
@@ -215,9 +273,9 @@ class ShowPickingList extends Component {
             <Title>{picking_list.shop_name} {picking_list.id}</Title>
           </Body>
           <Right>
-            <Button bordered primary small>
+            <Button bordered={!this.state.show_order} success small onPress={this.changeMode}>
               <Text>
-                {this.state.mode}
+                訂單
               </Text>
             </Button>
           </Right>
