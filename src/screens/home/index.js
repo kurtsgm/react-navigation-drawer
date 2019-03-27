3a5020a253431f65c0d1e226559fbb1bd96e3eff
Kurt <kurtsgm@gmail.com>
2019-03-27 19:57:57 +0800

    add version

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -110,7 +110,7 @@ class Home extends Component {
             </View>
           </Content>
           <Text style={{ backgroundColor: 'transparent',bottom: 0 ,textAlign:'right'}} >
-            版本: 1.0.3
+            版本: 1.0.51
           </Text>
         </ImageBackground>
       </Container>
