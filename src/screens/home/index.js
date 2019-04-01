c320211d73d8039f2319e779759b5841271dfe02
Kurt <kurtsgm@gmail.com>
2019-04-01 17:10:50 +0800

    minor refine

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -110,7 +110,7 @@ class Home extends Component {
             </View>
           </Content>
           <Text style={{ backgroundColor: 'transparent',bottom: 0 ,textAlign:'right'}} >
-            版本: 1.0.51
+            版本: 1.0.52
           </Text>
         </ImageBackground>
       </Container>
