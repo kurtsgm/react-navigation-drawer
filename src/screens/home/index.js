ec77fe4de0c4c2fd254ad2fa54f62f3330c664e4
kurt <kurtsgm@gmail.com>
2019-07-12 18:34:29 +0800

    change date

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -110,7 +110,7 @@ class Home extends Component {
             </View>
           </Content>
           <Text style={{ backgroundColor: 'transparent',bottom: 0 ,textAlign:'right'}} >
-            版本: 1.0.60 (2019/6/13)
+            版本: 1.0.60 (2019/7/12)
           </Text>
         </ImageBackground>
       </Container>
