60491ae8b1a124ea5e8f35ea2c41d127f6e3cde2
kurt <kurtsgm@gmail.com>
2019-11-15 00:30:45 +0800

    version

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -110,7 +110,7 @@ class Home extends Component {
             </View>
           </Content>
           <Text style={{ backgroundColor: 'transparent',bottom: 0 ,textAlign:'right'}} >
-            版本: 1.0.76 (2019/10/25)
+            版本: 1.0.77 (2019/11/15)
           </Text>
         </ImageBackground>
       </Container>
