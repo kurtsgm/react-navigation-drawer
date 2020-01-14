b9cf895cc05363d3cdc4ea583f03f5269e079f83
kurt <kurtsgm@gmail.com>
2020-01-14 21:19:28 +0800

    Revert "modify shelves token rules"
    This reverts commit f96ece3338549b9171e0b0efb898c1d8244d43d0.

--- src/common.js
+++ src/common.js
@@ -27,17 +27,41 @@ export function normalize_date(text){
 
 
 export function normalize_shelf_barcode(barcode){
-  return barcode.toUpperCase()
+  let tokens =[]
+  if(barcode.includes('-')){
+    if(barcode.includes(SDJ_SHELF_PREFIX)){
+      barcode = barcode.replace(SDJ_SHELF_PREFIX,"")
+    }
+    tokens = barcode.split('-').filter(e=> {
+      return e
+    }).map(e=> e.replace(/\D/g,''))
+    if(tokens[0].length == 1){
+      tokens[0] = `00${tokens[0]}`
+    }else if(tokens[0].length == 2){
+      tokens[0] = `0${tokens[0]}`
+    }
+  
+    barcode = tokens.join('')
+  }
+
+  tokens[0] = barcode.substring(0,3)
+  tokens[1] = barcode.substring(3,5)
+  tokens[2] = barcode.substring(5,6)
+  if(barcode.length>6){
+    tokens[3] = barcode.substring(6,7)
+  }
+  return tokens.filter(t=>t).join('-').toUpperCase()
 }
 
 export function getShelfLayer(token){
-  return parseInt(token.substring(5,7))
+  tokens = token.split('-').filter(e=>e)
+  return parseInt(tokens[2])
 }
 export function shelfSorter(shelf_a,shelf_b){
   try {
-    let shelf_a_tokens = shelf_a.substring(3,7)
-    let shelf_b_tokens = shelf_b.substring(3,7)
-    let result = parseInt(shelf_a_tokens) - parseInt(shelf_b_tokens)
+    let shelf_a_tokens = shelf_a.split('-')
+    let shelf_b_tokens = shelf_b.split('-')
+    let result = parseInt(`${shelf_a_tokens[0]}${shelf_a_tokens[1]}`) - parseInt(`${shelf_b_tokens[0]}${shelf_b_tokens[1]}`)
     return result
   } catch (e) {
     return 1
