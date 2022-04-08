4e39a84e7153bd588c236dfbc1e22fada53cfb0e
kurt <kurtsgm@gmail.com>
2022-04-08 15:50:04 +0800

    版號

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -111,7 +111,7 @@ class Home extends Component {
             </View>
           </Content>
           <Text style={{ backgroundColor: 'transparent',bottom: 0 ,textAlign:'right'}} >
-            版本: 1.1.41 (2022/03/17)
+            版本: 1.1.42 (2022/04/08)
           </Text>
         </ImageBackground>
       </Container>    
