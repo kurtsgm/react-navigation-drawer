597a9671e210c2ecf5f4eb8a8a63f86b71237747
kurt <kurtsgm@gmail.com>
2023-07-13 17:08:16 +0800

    stock taking list

--- src/screens/stock_taking/shelf.js
+++ src/screens/stock_taking/shelf.js
@@ -38,14 +38,21 @@ class StockTakingShelf extends Component {
   reload() {
     const { stock_taking, stock_taking_shelf } = this.props.navigation.state.params
 
-    apiFetch(GET_SHELF_INFO, { token: stock_taking_shelf.shelf.token,shop_id: stock_taking.shop_id }, shelf_data => {
-      apiFetch(GET_STOCK_TAKING_SHELF, { stock_taking_id: stock_taking.id, id: stock_taking_shelf.id }, (data) => {
-        items = shelf_data.storages.map(shelf_storage => {
-          console.log(shelf_storage)
-          return {
-            
+    apiFetch(GET_SHELF_INFO, { token: stock_taking_shelf.shelf.token, shop_id: stock_taking.shop_id }, shelf_data => {
+      apiFetch(GET_STOCK_TAKING_SHELF, { stock_taking_id: stock_taking.id, id: stock_taking_shelf.id }, (stock_taking_items) => {
+        items = shelf_data.storages
+        console.log(stock_taking_items)
+        for(let item of items){
+          let stock_taking_item = stock_taking_items.find(e => e.product_storage_id == item.product_storage_id)
+          if(stock_taking_item){
+            item.before_adjustment_pcs = stock_taking_item.before_adjustment_pcs
+            item.after_adjustment_pcs = stock_taking_item.after_adjustment_pcs
+          }else{
+            item.before_adjustment_pcs = item.pcs
+            item.after_adjustment_pcs = null
           }
-        })
+        }
+        this.setState({ items })
       })
     })
 
@@ -91,7 +98,24 @@ class StockTakingShelf extends Component {
                   <Grid>
                     <Row>
                       <Col size={1}>
+                        <Text>{`${item.product_storage.product.uid}\n${item.product_storage.product.name}`}</Text>
                       </Col>
+                      <Col size={1}>
+                        <Text>
+                          {[
+                            item.product_storage.storage_type_name,
+                            item.product_storage.expiration_date,
+                            item.product_storage.batch
+                          ].filter(e => e).join("\n")}
+                        </Text>
+                      </Col>
+                      <Col size={1}>
+                        {
+                          item.after_adjustment_pcs == null ? <Text>{`${item.before_adjustment_pcs}\n未盤`}</Text> :
+                            <Text style={item.after_adjustment_pcs == item.before_adjustment_pcs ? styles.green : styles.orange}>{`${item.after_adjustment_pcs} / ${item.before_adjustment_pcs}`}</Text>
+                        }
+                      </Col>
+
                     </Row>
                   </Grid>
                 </ListItem>
